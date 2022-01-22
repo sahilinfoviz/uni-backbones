542e40da48357bc2181c5e06959e721a6499c5c6
sahilinfoviz <sahilbiswaprakash.das@infoviz.co>
2022-01-22 10:08:19 +0530

    db.sql updated

--- db/db.sql
+++ db/db.sql
@@ -2,11 +2,13 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE users (
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    firstName TEXT NOT NULL,
+    lastName TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,
     phone VARCHAR(12) NOT NULL UNIQUE,
     isBlocked BOOLEAN DEFAULT false,
     iSDeleted BOOLEAN DEFAULT false,
-    password TEXT NOT NULL,
+    password TEXT,
     createdat timestamp without time zone default (now() at time zone 'utc'),
     LastUpdatedAt TIMESTAMPTZ
 );
