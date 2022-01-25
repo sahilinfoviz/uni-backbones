7dbfbe8a06ca92a4167b402b2a0fc3f69ba2e407
sahilinfoviz <sahilbiswaprakash.das@infoviz.co>
2022-01-25 09:28:13 +0530

    Fix: user_register.js

--- user_register.js
+++ user_register.js
@@ -11,32 +11,10 @@ const router = express.Router();
 //FOR REGISTRATION
 router.post('/register', async (req, res) => {
     try{
-        const { firstName, lastName, email, phone, mySecret, isTeacher, isStudent } = req.body;
-        const saltRounds = 12;
-        const salt = bcrypt.genSaltSync(saltRounds);
-        if(email && phone && mySecret){
-            if(testEmail(email)){
-                const result = await pool.query('SELECT * FROM users WHERE email = $1',[email]);
-                    if (result.rows.length === 0)
-                    {
-                        if(testPassword(mySecret)){
-                            const hashedPassword = await bcrypt.hash(mySecret, salt);
-                            if(testPhone(phone)){
-                                const myResult = await pool.query('SELECT * FROM users WHERE phone = $1',[phone]);
-                                if(myResult.rows.length === 0){
-                                const finalResult = await pool.query('INSERT INTO users (firstName,lastName,email,phone,mySecret) VALUES ($1,$2,$3,$4,$5) RETURNING *',
-                                [firstName,lastName,email,phone, mySecret]);
-                                await pool.query('INSERT INTO roles (id, isStudent, isTeacher) VALUES ($1,$2,$3) RETURNING *',[finalResult.rows[0].id,isStudent,isTeacher]);
-                                return res.json('successfully registered')
-                                } else return res.json('phone number already exists');
-                            } else return res.json('phone number is not in valid format');
-                        } else return res.json('password is not in valid format');
-                    }
-                    else return res.json('email already exists');        
-                }
-                else return res.json('email is not in valid format');
-        }
-        else return res.json('email or phone or password field can not be empty')
+        const { email, phone, mySecret } = req.body;
+        const finalResult = await pool.query('INSERT INTO users (email,phone,mySecret) VALUES ($1,$2,$3) RETURNING *',
+                                [email,phone, mySecret]);
+        res.json(finalResult.rows[0])
     }catch(err){
         res.status(500).json('registration unsuccessful');
     }
