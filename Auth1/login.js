5ce037accab4d710103de4042c9ed2b4ae0cc060
sahilinfoviz <sahilbiswaprakash.das@infoviz.co>
2022-01-27 17:26:50 +0530

    login.js updated

--- Auth1/login.js
+++ Auth1/login.js
@@ -1,107 +1,62 @@
 const express = require('express');
 require('dotenv').config();
-const bcrypt = require('bcrypt');
-
-const jwt = require('jsonwebtoken');
-
-const passport = require('passport');
-const passportJWT = require('passport-jwt');
-
-const pool = require('../db/db_config')
-
-// ExtractJwt to help extract the token
-let ExtractJwt = passportJWT.ExtractJwt;
-// JwtStrategy which is the strategy for the authentication
-let JwtStrategy = passportJWT.Strategy;
-let jwtOptions = {};
-
-jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-jwtOptions.secretOrKey = process.env.JWT_SECRET;
-
-
-const getAllUsers = async () => {
-  const user = await pool.query('SELECT * FROM users');
-  return user;
-};
-// lets create our strategy for web token
-let strategy = new JwtStrategy(jwtOptions, async function(jwt_payload, next) {
-    let user = await pool.query('SELECT * FROM users WHERE id = $1',[jwt_payload.id]);
-    if (user) {
-      next(null, user);
-    } else {
-      next(null, false);
-    }
-  });
-  // use the strategy
-  passport.use(strategy);
-  const router = express.Router();
-
-
-// login route
-router.post('/login', async function(req, res, next) { 
-  try{
-    const { email, password } = req.body;
-    if (email && password) {
-      let user = await pool.query('SELECT * FROM users WHERE email = $1',[email])
-      if (user.rows.length === 0) {
-        return res.json('No such user found');
-      } else {
-        // Valid password check
-    const correctPassword = await bcrypt.compare(password, user.rows[0].password);
-    if (!correctPassword) return res.json('incorrect password');
-    else{
-        const result = await pool.query('SELECT * FROM roles where id = $1',[user.rows[0].id]);
-        let teacher = result.rows[0].isteacher;
-        let student = result.rows[0].isstudent; 
-        let payload = user.rows[0];
-        let token = jwt.sign(payload, jwtOptions.secretOrKey,{ expiresIn: '30m' });
-        res.set('Access-Control-Allow-Origin', '*');
-        res.json({ msg: 'ok', token: token , isTeacher: teacher, isStudent: student});
-    }
-      } 
-    } else {
-      return res.json('email or password field can not be empty');
-    }
-  }catch(err){
-        res.status(500).json('login unsuccessful');
-  }  
-  });
-  
-// To check PASSPORT authentication strategy working or not 
-  
+const myJwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const jwtDecode = require('jwt-decode');
+const db = require('../db/db_config');
+const bcrypt = require('bcryptjs/dist/bcrypt');
+const app = express();
+app.use(cookieParser());
 
 
 
-// To check authorization for teacher role
-
-router.get('/users/:email',passport.authenticate('jwt', { session: false }),async function(req, res) {
-  try{
-    const myParam = req.params.email
-    const result = await pool.query('SELECT * FROM users WHERE email = $1',[myParam]);
-    const myRole = await pool.query('SELECT * FROM roles WHERE id = $1',[result.rows[0].id])
-    if(myRole.rows[0].isteacher === true){
-      getAllUsers().then(user => res.status(200).json(user.rows)); 
-    }
-    else return res.json('not authorized to perform this action')
-  }catch(err){
-    res.status(500).json('error ocurred')
-  }
-  })
-
-  // To check authorization for student role
-
-  router.get('/allUser/:email',passport.authenticate('jwt', { session: false }),async function(req, res) {
+app.post('/login', async (req, res) => {
     try{
-      const myParam = req.params.email
-      const result = await pool.query('SELECT * FROM users WHERE email = $1',[myParam]);
-      const myRole = await pool.query('SELECT * FROM roles WHERE id = $1',[result.rows[0].id])
-      if(myRole.rows[0].isstudent === true){
-        getAllUsers().then(user => res.status(200).json(user.rows)); 
-      }
-      else return res.json('not authorized to perform this action')
-    }catch(err){
-      res.status(500).json('error ocurred')
-    }
-    })
+        const { email, password } = req.body;
+        const user = await db.query('SELECT * FROM users WHERE email = $1',[email]);
+        if(!user){
+            return res.status(403).json({message: "wrong email or password"});
+        }
+        const myRoles = await db.query('SELECT * FROM roles WHERE id = $1',[user.rows[0].id]);
+        let role
+        if (myRoles.rows[0].isteacher === true)
+            role = 'teacher';
+        else role = 'student';
+        const passwordValidate = await bcrypt.compare(password,user.rows[0].password);
+        if(passwordValidate) {
+            const data = {
+                fistname: user.rows[0].firstname,
+                lastname: user.rows[0].lastname,
+                id: user.rows[0].id,
+                phone: user.rows[0].phone,
+                userRole: role
+            };
+            const userInfo = Object.assign({},data);
+            const token = myJwt.sign(userInfo,process.env.JWT_SECRET,{ algorithm: 'HS256', expiresIn: '1h'});
 
-  module.exports = router;
+            const decodedToken = jwtDecode(token);
+            const expiresAt = decodedToken.exp;
+            res.cookie('token', token, {
+                httpOnly: true
+            });
+            res.json({
+                message: 'Authentication Successful',
+                userInfo,
+                expiresAt
+            });
+        } else {
+            res.status(403).json({
+                message: 'wrong email or password.'
+            });
+        }
+    }catch(err) {
+        console.log(err);
+        return res.status(400)
+                  .json({message: 'Something went wrong.'});
+    }
+});
+
+
+
+
+module.exports = app;
