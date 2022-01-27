bb01757334604b2f6a9a27d103a62483fd836ec3
sahilinfoviz <sahilbiswaprakash.das@infoviz.co>
2022-01-27 17:18:04 +0530

    api.js updated

--- api.js
+++ api.js
@@ -1,20 +1,22 @@
 const express = require('express');
+const compression = require('compression');
+require('dotenv').config();
 const helmet = require('helmet');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
-const jwt = require('jsonwebtoken');
-
-const passport = require('passport');
-
-const userRouter = require('./user_register')
+const jwt = require('express-jwt');
+const cookieParser = require('cookie-parser');
+const registerRouter = require('./register');
 const loginRouter = require('./Auth1/login');
+const myRoutes = require('./Auth1/route');
+const csrf = require('csurf');
+const csrfProtection = csrf({
+    cookie: true
+});
 
 const app = express();
+app.use(compression());
 app.use(helmet());
-
-// initialize passport with express
-app.use(passport.initialize());
 
 const corsOptions = { credentials: true, origin: process.env.URL || '*' };
 app.use(cors(corsOptions));
@@ -23,18 +25,27 @@ app.use(cors());
 app.use(bodyParser.json());
 //parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
+app.use('/new',registerRouter);
+app.use('/api',loginRouter);
 
-
-app.use('/api', userRouter)
-app.use('/auth', loginRouter)
-
-// add a basic route
-app.get('/welcome', function(req, res) {
+app.get('/welcome', (req, res) => {
     res.json({ message: 'Express is up!' });
   })
-require('dotenv').config();
+const checkJwt = jwt({
+    secret: process.env.JWT_SECRET,
+    getToken: req => req.cookies.token,
+    algorithms: ['HS256']
+});
 
-const PORT = process.env.PORT;
+app.use(csrfProtection);
+
+app.get('/api/csrf-token', (req, res) => {
+    res.json({ csrfToken: req.csrfToken() });
+});
+
+app.use('/api/v1',checkJwt,myRoutes);
 
 module.exports = app;
+
