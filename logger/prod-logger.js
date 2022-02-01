1517990e61596003fb7b8fb53915eabca6db3e4a
sahilinfoviz <sahilbiswaprakash.das@infoviz.co>
2022-02-01 18:53:47 +0530

    feat: aws secrets added to .env file

--- logger/prod-logger.js
+++ logger/prod-logger.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const { format, createLogger, transports } = require('winston');
 const { timestamp, combine, errors, json } = format;
 const S3StreamLogger = require('s3-streamlogger').S3StreamLogger;
  
 const s3_stream = new S3StreamLogger({
              bucket: "authemorod",
-      access_key_id: "AKIAYY4ZQQHZPHDNRWZI",
-  secret_access_key: "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"
+      access_key_id: process.env.access_key_id,
+  secret_access_key: process.env.secret_access_key
 });
 const transport = new (transports.Stream)({
     stream: s3_stream
