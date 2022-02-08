af420e27d39d2a714b3b46214a981d87ea738675
sahilinfoviz <sahilbiswaprakash.das@infoviz.co>
2022-02-08 06:53:03 +0530

    feat: .env file removed

--- logger/prod-logger.js
+++ logger/prod-logger.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 
 // const s3_stream = new S3StreamLogger({
 //              bucket: process.env.AWS_BUCKET_NAME,
-//       access_key_id: process.env.ACCESS_KEY,
+//       access_key_id: process.env.ACC_KEY,
 //   secret_access_key: process.env.SECRET_KEY_ID
 // });
 // const transport = new (transports.Stream)({
